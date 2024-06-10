aec827a7f01e7cd5d0836438694d94bad9a5ea67
Ankit <smpratik888@gmail.com>
2024-06-10 15:38:41 +0530

    update02

--- src/App.jsx
+++ src/App.jsx
@@ -24,20 +24,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept 
-             title={CORE_CONCEPTS[0].title}
-             description={CORE_CONCEPTS[0].description}
-             image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-            {...CORE_CONCEPTS[1]}  
-             />
-            <CoreConcept 
-            {...CORE_CONCEPTS[2]}
-            />
-            <CoreConcept
-            {...CORE_CONCEPTS[3]}
-            />
+          {CORE_CONCEPTS.map((conceptItem) => (
+            <CoreConcept key={conceptItem.title} {...conceptItem} />
+          ))}
           </ul>
         </section>
         <section id="examples">
